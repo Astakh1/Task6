090287d5945c829a9765b65085974d691b675d76
lendingfox <mail@some.com>
2025-12-19 15:46:21 +0300

    Add simple temperature converter

---
+++ converter.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+static double celsiusToFahrenheit(double c);
+static double fahrenheitToCelsius(double f);
+static double celsiusToKelvin(double c);
+static double kelvinToCelsius(double k);
+static double fahrenheitToKelvin(double f);
+static double kelvinToFahrenheit(double k);
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "converter.h"
+
+int main()
+{
+    std::cout << celsiusToFahrenheit(30) << std::endl;
+    std::cout << fahrenheitToKelvin(100) << std::endl;
+}
